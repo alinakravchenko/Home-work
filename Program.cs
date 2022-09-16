da01fd801b7123b93958810690a8cecde4614876
jony <jony@DESKTOP-LF5LBL2>
2022-09-16 11:52:23 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿#region TASK1
+//int num=0;
+//do
+//{
+//    Console.WriteLine("Введите число: ");
+//    try
+//    {
+//        num = Int32.Parse(Console.ReadLine());
+//        if (num < 0 || num > 100)
+//        {
+//            throw new Exception();
+//        }
+//    }
+//    catch (Exception)
+//    {
+//        Console.WriteLine("Введено неправильное число!");
+//    }
+
+//    if (num % 3 == 0 && num % 5 == 0)
+//    {
+//        Console.WriteLine("Fizz Buzz");
+//        return;
+//    }
+//    if (num % 3 != 0 && num % 5 != 0)
+//    {
+//        Console.WriteLine(num);
+//    }
+//    else if (num % 3 == 0)
+//    {
+//        Console.WriteLine("Fizz");
+//    }
+//    else if (num % 5 == 0)
+//    {
+//        Console.WriteLine("Buzz");
+//    }
+
+//} while (num < 0 || num > 100);
+#endregion //TASK1 DONE
+
+#region TASK2
+//int num = 0;
+//int i = 0;
+
+//Console.WriteLine("Введите число: ");
+//try
+//{
+//    num = int.Parse(Console.ReadLine());
+//    Console.WriteLine("Введите число для процента: ");
+//    i = int.Parse(Console.ReadLine());
+
+//    int result = (num * i) / 100;
+//    Console.WriteLine("Результат {0}", result);
+//}
+//catch (Exception)
+//{
+//    Console.WriteLine("Введено неправильно число!");
+
+//}
+#endregion //TASK2 DONE
+
+#region TASK3
+//double num1, num2, num3, num4;
+//int result = 0;
+
+//Console.WriteLine("Введите первое число");
+//num1 = Double.Parse(Console.ReadLine());
+//Console.WriteLine("Введите второе число");
+//num2 = Double.Parse(Console.ReadLine());
+//Console.WriteLine("Введите третье число");
+//num3 = Double.Parse(Console.ReadLine());
+//Console.WriteLine("Введите четвертое число");
+//num4 = Double.Parse(Console.ReadLine());
+
+//result = (((result + (int)num1)*10 + (int)num2)*10 + (int)num3)*10+(int)num4;
+//Console.WriteLine("Результаt:  {0}", result);
+#endregion //TASK3 DONE
+
